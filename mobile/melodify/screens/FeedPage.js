1961261b287b674568c530ca305394d5cc8e8685
ramazanoacar <ramazan.acar@boun.edu.tr>
2024-04-26 13:21:46 +0300

    enhance feedpage

--- mobile/melodify/screens/FeedPage.js
+++ mobile/melodify/screens/FeedPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import GradientBackground from '../components/GradientBackground';
 
 const FeedPage = () => {
     return (
         <GradientBackground>
-            <View style={styles.container}>
+            <Text style={styles.mainTitle}>Melodify</Text>
+            <View style={styles.searchContainer}>
+                <TextInput 
+                    style={styles.input} 
+                    placeholder="Search" 
+                    placeholderTextColor="#ccc"
+                />
+                <Ionicons name="search" size={24} color="gray" style={styles.iconStyle} />
+            </View>
+            <View style={styles.container2}>
                 <Text style={styles.title}>Feed Page</Text>
                 <Text style={styles.content}>Welcome to the Feed!</Text>
             </View>
@@ -14,11 +24,41 @@ const FeedPage = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
+    mainTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginTop: 30,
+        marginLeft: 20,
+        alignSelf: 'flex-start',
+    },
+    searchContainer: {
+        alignSelf: 'flex-start',
+        width: '100%',
+        padding: 20,
+        position: 'relative',
+    },
+    input: {
+        backgroundColor: 'white',
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 40, // Adjust padding to make space for the icon inside the box
+        borderRadius: 10,
+        marginBottom: 10,
+        width: '100%',
+    },
+    iconStyle: {
+        position: 'absolute',
+        right: 30, // Adjust based on your design preference
+        top: 33, // Adjust based on the height of your TextInput
+        backgroundColor: 'transparent',
+    },
+    container2: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
+        padding: 20,
     },
     title: {
         fontSize: 24,
@@ -29,7 +69,7 @@ const styles = StyleSheet.create({
     content: {
         fontSize: 18,
         color: 'white',
-    }
+    },
 });
 
 export default FeedPage;
