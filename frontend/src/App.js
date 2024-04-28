b0a58fc5ffa16ef4bef6466a6c7288af8ef9b0b9
guney <guney@underheavenstudios.com>
2024-04-29 00:26:58 +0300

    Added an empty feed page to route to once we implement "sign in"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
+import FeedPage from './pages/FeedPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
                   <Route path="/" element={<Navigate replace to="/register" />} />
                   <Route path="/register" element={<RegistrationPage />} />
                   <Route path="/login" element={<LoginPage />} />
+                  <Route path="/feed" element={<FeedPage />} />
               </Routes>
           </div>
       </Router>
---
+++ frontend/src/pages/FeedPage.js
@@ -0,0 +1,13 @@
+function FeedPage() {
+    return (
+        <>
+            <div className="flex flex-col sm:w-full max-w-[360px] bg-[#111927CC] 
+            rounded-2xl p-6 shadow-[0_-4px_8px_-2px_rgba(0,0,0,0.25)] backdrop-blur w-5/6
+            ">
+                <p className="text-[28px] text-white text-center mb-6 tracking-tight leading-8 font-medium">Welcome to Melodify!!! Your feed is empty at the moment.</p>
+            </div>
+        </>
+    );
+}
+
+export default FeedPage;
