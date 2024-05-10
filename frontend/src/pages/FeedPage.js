5db411b5c1cfd8eed8cfc3f5042081e7a2166200
guney <guney@underheavenstudios.com>
2024-05-10 14:51:32 +0300

    Added Preview component with mock data. Frontend is functional - connection with end points needed.

--- frontend/src/pages/FeedPage.js
+++ frontend/src/pages/FeedPage.js
@@ -2,50 +2,92 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Post from "../components/Post";
 import CreatePostDialog from "../components/CreatePostDialog";
+import ResultPreview from "../components/SearchResultPreview";
 import { FaPlus } from "react-icons/fa";
 import Navbar from "../components/Navbar";
 
 const mockPost = {
-  pp: "pp2.jpeg",
-  username: "The Lumineers",
-  when: "2 hours ago",
-  textBody: "Hey everyone! We're excited to announce our new album coming out next month. Stay tuned for more updates!",
-  imageURL: "content.jpg",
+    pp: "pp2.jpeg",
+    username: "The Lumineers",
+    when: "2 hours ago",
+    textBody: "Hey everyone! We're excited to announce our new album coming out next month. Stay tuned for more updates!",
+    imageURL: "content.jpg",
 }
 
 function FeedPage() {
-  const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
+    const [searchQuery, setSearchQuery] = useState("");
+    const [createPostDialogOpen, setCreatePostDialogOpen] = useState(false);
+    const [posts, setPosts] = useState([mockPost, mockPost, mockPost]);
 
-  const [createPostDialogOpen, setCreatePostDialogOpen] = useState(false);
-  const [posts, setPosts] = useState([mockPost, mockPost, mockPost]);
+    const mockResults = [
+        { id: 1, name: "Adam Acker", imageURL: "ProfilePicture1.png" },
+        { id: 2, name: "Jessie Jordan", imageURL: "ProfilePicture2.png" },
+        { id: 3, name: "Kareem Kahil", imageURL: "ProfilePicture3.png" },
+        { id: 4, name: "Glenn Garrison", imageURL: "ProfilePicture4.png" },
+        { id: 5, name: "Melina Maxwell", imageURL: "ProfilePicture5.png" }
+    ];
+    const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-    // You can perform any search-related logic here, such as filtering or fetching data based on the search query
-  };
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+        if (event.target.value) {
+            setSearchResults(mockResults);
+        } else {
+            setSearchResults([]);
+        }
+    };
+
+
+    const handleSearchKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
+            console.log("Search for:", searchQuery);
+        }
+    };
+
 
     const handleLogout = () => {
         navigate("/login");
     };
 
-  const addPost = (newPost) => {
-    setPosts([newPost, ...posts]);
-  }
+    const addPost = (newPost) => {
+        setPosts([newPost, ...posts]);
+    }
 
-  return (
-    <>
-      <div className="flex justify-center bg-[#0D1520] p-4 w-screen">
-        <div className="w-[400px] overflow-scroll h-screen flex flex-col items-center
-        no-scrollbar">
-          {posts && posts.map((post, index) => <Post key={index} postData={post}></Post>)}
-        </div>
-      </div>
-      <CreatePostDialog isOpen={createPostDialogOpen} setIsOpen={setCreatePostDialogOpen} addPost={addPost}></CreatePostDialog>
-      <button onClick={() => setCreatePostDialogOpen(true)} className="fixed bottom-4 right-4 bg-[#2870BD] hover:bg-[#0090FF] text-white flex items-center transition-colors duration-300 pl-4 pr-6 py-2 rounded-md cursor-pointer"><FaPlus className="inline-block mr-2 z-0"></FaPlus>Create Post</button>
-      <Navbar></Navbar>
-    </>
-  );
+    return (
+        <>
+            <div className="w-screen h-screen overflow-hidden bg-[#0D1520] flex items-center justify-center">
+                <div className="flex justify-center bg-[#0D1520] p-4 w-screen h-screen">
+                    <div className="w-[400px] overflow-scroll h-screen flex flex-col items-center no-scrollbar">
+                        {posts && posts.map((post, index) => <Post key={index} postData={post}></Post>)}
+                    </div>
+                </div>
+                <CreatePostDialog isOpen={createPostDialogOpen} setIsOpen={setCreatePostDialogOpen} addPost={addPost}></CreatePostDialog>
+                <button onClick={() => setCreatePostDialogOpen(true)} className="fixed bottom-4 right-4 bg-[#2870BD] hover:bg-[#0090FF] text-white flex items-center transition-colors duration-300 pl-4 pr-6 py-2 rounded-md cursor-pointer"><FaPlus className="inline-block mr-2 z-0"></FaPlus>Create Post</button>
+                <Navbar></Navbar>
+                <div className="absolute top-4 right-4 rounded-md py-2 px-4 w-1/6 rounded-md no-scrollbar border border-gray-300 focus:outline-none focus:border-indigo-500">
+                    <div className="flex flex-col items-center w-full">
+                        <input
+                            type="text"
+                            placeholder="Search for users"
+                            className="py-2 px-4 w-full rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500 mb-4"
+                            value={searchQuery}
+                            onChange={handleSearchChange}
+                            onKeyPress={handleSearchKeyPress}
+                        />
+                        <div className="w-full bg-[#FFFFFF] rounded-md">
+                            {searchResults.map(result => (
+                                <ResultPreview key={result.id} result={result} />
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default FeedPage;
--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -2,64 +2,73 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function SearchPage() {
-    const location = useLocation(); // Hook to get location object
+    const location = useLocation();
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState([]);
 
-    // Extract the search query from the URL query parameters
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
         const query = queryParams.get('query');
         if (query) {
             setSearchQuery(query);
-            handleSearch(query);  // Optionally perform the search immediately
+            handleSearch(query);
         }
     }, [location]);
 
+
+
+    const mockResults = [
+        { id: 1, name: "Adam Acker", imageURL: "ProfilePicture1.png" },
+        { id: 2, name: "Jessie Jordan", imageURL: "ProfilePicture2.png" },
+        { id: 3, name: "Kareem Kahil", imageURL: "ProfilePicture3.png" },
+        { id: 4, name: "Glenn Garrison", imageURL: "ProfilePicture4.png" },
+        { id: 5, name: "Melina Maxwell", imageURL: "ProfilePicture5.png" }
+    ];
+
     const handleSearchChange = (event) => {
         setSearchQuery(event.target.value);
+        if (event.target.value) {
+            setSearchResults(mockResults);
+        } else {
+            setSearchResults([]);
+        }
     };
 
     const handleSearch = (query = searchQuery) => {
         console.log("Searching for:", query);
-
-        fetch(`http://34.118.44.165/search?query=${encodeURIComponent(query)}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP status ${response.status}`);
-                }
-                console.log(response);
-                return response.body;
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
+        setSearchResults(mockResults);
     };
 
     return (
-        <div className="mt-4 flex flex-col w-1/4 max-w-4xl mx-auto">
-            <div className="flex">
-                <input
-                    type="text"
-                    placeholder="Search for songs, artists, etc."
-                    className="py-2 px-4 w-full rounded-l-md border border-gray-300 focus:outline-none focus:border-indigo-500"
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                />
-                <button
-                    className="bg-indigo-500 text-white py-2 px-4 rounded-r-md border border-indigo-500 hover:bg-indigo-600"
-                    onClick={() => handleSearch()}
-                >
-                    Search
-                </button>
+        <div className="w-screen h-screen bg-[#0D1520] flex items-center justify-center">
+            <div className="mt-4 flex flex-col w-1/4 max-w-4xl mx-auto">
+                <div className="flex">
+                    <input
+                        type="text"
+                        placeholder="Search for users"
+                        className="py-2 px-4 w-full rounded-l-md border border-gray-300 focus:outline-none focus:border-indigo-500"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                    />
+                    <button
+                        className="bg-indigo-500 text-white py-2 px-4 rounded-r-md border border-indigo-500 hover:bg-indigo-600"
+                        onClick={() => handleSearch()}
+                    >
+                        Search
+                    </button>
+                </div>
+                <ul className="mt-4 bg-white shadow overflow-hidden rounded-md">
+                    {searchResults.map(result => (
+                        <li key={result.id} className="px-4 py-2 border-b border-gray-200 flex items-center last:border-b-0">
+                            <img
+                                src={result.imageURL}
+                                alt="Profile"
+                                className="w-10 h-10 rounded-full mr-4"
+                            />
+                            {result.name}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
