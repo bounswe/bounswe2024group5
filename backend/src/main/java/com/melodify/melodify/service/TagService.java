0e6913425746add495252f51baecc95f3b02cd58
Semih YILMAZ <semihyilmaz37@proton.me>
2024-05-12 01:12:34 +0300

    implement Tag Classes and revise database for tag class

---
+++ backend/src/main/java/com/melodify/melodify/service/TagService.java
@@ -0,0 +1,28 @@
+package com.melodify.melodify.service;
+
+import com.melodify.melodify.model.Post;
+import com.melodify.melodify.model.Tag;
+import com.melodify.melodify.repository.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TagService {
+
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> getTagsByPost(Post post) {
+        return tagRepository.findByPost(post);
+    }
+
+    public List<Post> getPostsByTag(String tag) {
+        return tagRepository.findByTag(tag).stream().map(Tag::getPost).collect(Collectors.toList());
+    }
+
+}
