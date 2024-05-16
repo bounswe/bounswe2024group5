743e6692e7e75d15050e282a400bcb9227abf657
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2024-05-17 00:05:01 +0300

    implement tag delete

--- frontend/src/components/CreatePostDialog.js
+++ frontend/src/components/CreatePostDialog.js
@@ -33,6 +33,10 @@ function CreatePostDialog({ isOpen, setIsOpen, addPost }) {
         setTagInput("");
     }
 
+    const handleDeleteTag = (tag) => {
+        setTags(tags.filter(t => t !== tag));
+    }
+
     const handlePost = () => {
         /*
         addPost({
@@ -105,7 +109,7 @@ function CreatePostDialog({ isOpen, setIsOpen, addPost }) {
                 <textarea value={textBody} onChange={handleTextBodyChange} className="w-full h-[140px] bg-[#0D1520] px-3 py-2 resize-none outline-none rounded-lg no-scrollbar" placeholder="What's on your mind?"></textarea>
                 { selectedImage && <img src={URL.createObjectURL(selectedImage)} alt="Selected" className="w-full h-[200px] object-cover mt-4 rounded-lg"/> }
                 <div className="flex flex-wrap gap-2">
-                        {tags && tags.map((tag, index) => <div key={index} className="text-white bg-[#0D1520] w-fit px-4 py-2 rounded-md">#{tag}</div>)}
+                        {tags && tags.map((tag, index) => <div key={index} className="text-white bg-[#0D1520] w-fit pl-4 pr-2 py-2 rounded-md flex gap-2 items-center"><span>#{tag}</span><button onClick={() => {handleDeleteTag(tag)}} className="bg-[#0D2847] hover:bg-[#103056] flex w-6 h-6 items-center justify-center rounded-full transition-colors duration-300 hover:cursor-pointer">x</button></div>)}
                 </div>
                 <div className="pt-2">
                     <input value={tagInput} onChange={(e) => {setTagInput(e.target.value)}} className="bg-[#0D1520] outline-none py-2 px-4 rounded-l-lg" placeholder="Tag a topic."></input>
