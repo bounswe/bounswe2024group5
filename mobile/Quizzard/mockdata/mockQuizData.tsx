a5d7cddc865f007a30d6f4b0683a18f81089fb96
Ebrar Kızıloğlu <ebrarkiziloglu@gmail.com>
2024-10-14 15:40:50 +0300

    Create mock data for the quiz view on home screen

---
+++ mobile/Quizzard/mockdata/mockQuizData.tsx
@@ -0,0 +1,26 @@
+
+const mockQuizData = [
+    {
+      image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1e8dffe4-8cd4-4781-9211-7e359b28baef/d4arkeq-ec26119f-b712-40bd-ae34-b1b44c28054f.jpg/v1/fit/w_512,h_512,q_70,strp/chibi_animals_by_fennemusic_d4arkeq-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTEyIiwicGF0aCI6IlwvZlwvMWU4ZGZmZTQtOGNkNC00NzgxLTkyMTEtN2UzNTliMjhiYWVmXC9kNGFya2VxLWVjMjYxMTlmLWI3MTItNDBiZC1hZTM0LWIxYjQ0YzI4MDU0Zi5qcGciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.fPB6zDqVrnZQDKQ-BU7PilsEKbOigiBzoLXkM82kf-w',
+      title: 'Animals',
+      questions: 43,
+      level: 'A2',
+      likes: 451,
+    },
+    {
+      image: 'https://i.pinimg.com/236x/35/6f/6f/356f6fa46cd2cb61a612e2d8fd427b9d.jpg',
+      title: 'Kitchen Utensils From France',
+      questions: 12,
+      level: 'A2',
+      likes: 352,
+    },
+    {
+      image: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043233-anime-away-face-no-nobody-spirited_113254.png',
+      title: 'Anime',
+      questions: 25,
+      level: 'A2',
+      likes: 128,
+    },
+  ];
+
+export default mockQuizData;
