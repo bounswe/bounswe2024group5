d7608263af270afaa8396e85e709057d7612651c
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-17 00:49:00 +0300

    fix index page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ErrorPage } from "./pages/error-page";
 import { ForumPage } from "./pages/forum-page";
 import { ListQuizzesPage } from "./pages/list-quiz-page";
-import { MainPage } from "./pages/main-page";
+
 import { SolveQuizPage } from "./pages/solve-quiz-page";
 
 import { RootLayout } from "./layouts/root-layout";
@@ -16,7 +16,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
-          <Route index element={<MainPage />} />
+          <Route index element={<ListQuizzesPage />} />
           <Route path="quizzes" element={<ListQuizzesPage />} />
           <Route path="quiz/:quizId" element={<SolveQuizPage />} />
           <Route path="forum" element={<ForumPage />} />
