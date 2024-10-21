73aa86ebcd6e845b044a6d660fc02cff8ac54c0b
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-21 22:05:38 +0300

    fix: use hostUrl instead of static variable

--- frontend/src/hooks/api/get-quizzes.ts
+++ frontend/src/hooks/api/get-quizzes.ts
@@ -1,13 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
 import { Quiz } from '../../types/question'
+import { useContext } from 'react'
+import HostContext from '../../HostContext'
 
 export const useFetchQuizzes = () => {
 	const TOKEN = sessionStorage.getItem('token')
+	const hostUrl = useContext(HostContext)
 	const context = useQuery({
 		queryKey: ['quizzes'],
 		queryFn: async () => {
 			console.log("FETCHING QUIZZES")
-			const body = await fetch('http://localhost:80/api/quizzes', {
+			const body = await fetch(`${hostUrl}/api/quizzes`, {
 				headers: {
 					'Authorization': `Bearer ${TOKEN}`,
 					'Content-Type': 'application/json'
