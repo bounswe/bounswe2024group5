9344a026055d00c340fbca91eb557bd23e0c1b75
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-21 22:24:33 +0300

    fix card height

--- frontend/src/components/list-quizzes/featured-quiz-card.tsx
+++ frontend/src/components/list-quizzes/featured-quiz-card.tsx
@@ -11,10 +11,10 @@ export const FeaturedQuizCard = ({ quiz }: { quiz: Quiz }) => (
       transition={{ duration: 0.5 }}
       whileHover={{ scale: 1.03 }}
       whileTap={{ scale: 0.98 }}
-      className="flex flex-col pb-6 bg-purple-100 shadow-md cursor-pointer rounded-3xl"
+      className="flex flex-col pb-2 bg-purple-100 shadow-md cursor-pointer rounded-3xl"
     >
       <div className="w-full bg-white min-h-32 rounded-t-3xl" />
-      <div className="flex flex-col justify-between flex-grow h-40 p-4">
+      <div className="flex flex-col justify-between flex-grow p-4 min-h-40">
         <div>
           <h3 className="mb-2 text-xl font-bold text-purple-800">
             {quiz.title}
