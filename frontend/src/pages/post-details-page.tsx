90263e85a87915c0be1053060ee19e073a5518eb
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2024-11-04 16:33:17 +0300

    implement component for writing replies to posts

--- frontend/src/pages/post-details-page.tsx
+++ frontend/src/pages/post-details-page.tsx
@@ -2,8 +2,13 @@ import { useLocation } from "react-router-dom";
 import { useFetchPost } from "../hooks/api/get-forum-post";
 import { ForumPostComponent } from "../components/forum/ForumPost";
 import { ForumReplyComponent } from "../components/forum/ForumReply";
+import { CreateReplyComponent } from "../components/forum/CreateReply";
+import { useState } from "react";
 
 export const PostDetailsPage = () => {
+
+    const [replyComponent, setReplyComponent] = useState<boolean>(false);
+
     const currentPath = useLocation().pathname;
 
     const postId = currentPath.split("/").pop();
@@ -11,12 +16,31 @@ export const PostDetailsPage = () => {
     const { data, isLoading } = useFetchPost(parseInt(postId || "0"));
     if (isLoading) return <p>Loading...</p>
 
+    const closeReplyComponent = () => {
+        setReplyComponent(false)
+    }
+
+    const openReplyComponent = () => {
+        setReplyComponent(true)
+    }
+
+    const sendReply = () => {
+        alert("Your replies is sent.")
+        setReplyComponent(false)
+    }
+
     return (
         <>
             <div className="flex flex-col gap-8 mx-auto items-center">
                 { data?.post?.map(mockPost => {
-                    return <ForumPostComponent post={mockPost} />
+                    return <ForumPostComponent post={mockPost}/>
                 })}
+                { !replyComponent && 
+                    <div className="w-full max-w-2xl h-fit flex justify-end">
+                        <button onClick={openReplyComponent} className="bg-violet-600 text-white p-2 px-8 rounded-lg">Reply</button>
+                    </div>
+                }
+                { replyComponent &&  <CreateReplyComponent close={closeReplyComponent} send={sendReply} />}
                 { data?.replies?.map(mockReply => {
                     return <ForumReplyComponent reply={mockReply} />
                 })}
