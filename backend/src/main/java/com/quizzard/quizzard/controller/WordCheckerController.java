790ff1422d0bedaef1192a24b30c4a373ecbb0fd
Semih YILMAZ <semihyilmaz37@proton.me>
2024-11-24 01:24:59 +0300

    feat(backend): added turkish words to wordchecker endpoint

--- backend/src/main/java/com/quizzard/quizzard/controller/WordCheckerController.java
+++ backend/src/main/java/com/quizzard/quizzard/controller/WordCheckerController.java
@@ -1,6 +1,7 @@
 package com.quizzard.quizzard.controller;
 
 import com.quizzard.quizzard.repository.EnglishRepository;
+import com.quizzard.quizzard.repository.TurkishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +18,26 @@ public class WordCheckerController {
     @Autowired
     private EnglishRepository englishRepository;
 
+    @Autowired
+    private TurkishRepository turkishRepository;
+
     @GetMapping()
-    public ResponseEntity<Map<String, Object>> checkWord(@RequestParam String word, @RequestParam String type) {
-        if (englishRepository.existsByWord(word))
-            return ResponseEntity.ok(Map.of("isValid", true));
-        else
-            return ResponseEntity.ok(Map.of("isValid", false));
+    public ResponseEntity<Map<String, Object>> checkWord(@RequestParam String word, @RequestParam String language) {
+        if(language.equals("turkish")) {
+            if (turkishRepository.existsByWord(word))
+                return ResponseEntity.ok(Map.of("isValid", true));
+            else
+                return ResponseEntity.ok(Map.of("isValid", false));
+        }
+        else if(language.equals("english")) {
+            if (englishRepository.existsByWord(word))
+                return ResponseEntity.ok(Map.of("isValid", true));
+            else
+                return ResponseEntity.ok(Map.of("isValid", false));
+        }
+        else {
+            return ResponseEntity.badRequest().body(Map.of("error", "Invalid type"));
+        }
     }
 
 }
