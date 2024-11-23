615ac7af5890cb66b1eac37ece119f197e5c198d
Süleyman Emir Taşan <141218519+set120120@users.noreply.github.com>
2024-11-23 18:04:52 +0300

    Added old version of init.sql

---
+++ database/1-init.sql
@@ -0,0 +1,171 @@
+-- 1728826462-init.sql
+
+-- Create the database (if not exists)
+CREATE DATABASE IF NOT EXISTS quizzard;
+
+-- Use the database
+USE quizzard;
+
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    profile_picture TEXT,
+    english_proficiency INT NOT NULL,
+    points INT DEFAULT 0,
+    created_quizzes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create a quizzes table
+CREATE TABLE IF NOT EXISTS quizzes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    image TEXT,
+    difficulty FLOAT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Create a questions table
+CREATE TABLE IF NOT EXISTS questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_id INT NOT NULL,
+    question_type ENUM('english_to_turkish', 'turkish_to_english', 'english_to_sense') NOT NULL,
+    word VARCHAR(255) NOT NULL,
+    correct_answer VARCHAR(255) NOT NULL,
+    wrong_answer1 VARCHAR(255),
+    wrong_answer2 VARCHAR(255),
+    wrong_answer3 VARCHAR(255),
+    difficulty FLOAT,
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+);
+
+-- Create a quiz_history table
+CREATE TABLE IF NOT EXISTS quiz_attempts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    quiz_id INT NOT NULL,
+    score INT,
+    is_completed BOOLEAN DEFAULT FALSE,
+    completed_at TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS question_answers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_attempt_id INT NOT NULL,
+    question_id INT NOT NULL,
+    answer VARCHAR(255),
+    correct BOOLEAN,
+    answered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (quiz_attempt_id, question_id),
+    FOREIGN KEY (quiz_attempt_id) REFERENCES quiz_attempts(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
+);
+
+-- Create a forum_posts table
+CREATE TABLE IF NOT EXISTS forum_posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255),
+    content TEXT,
+    upvote INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Create a forum_replies table
+CREATE TABLE IF NOT EXISTS forum_replies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES forum_posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- Create a favorite_quizzes table
+CREATE TABLE IF NOT EXISTS favorite_quizzes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    quiz_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id) ON DELETE CASCADE,
+    UNIQUE (user_id, quiz_id)
+);
+
+-- Create a favorite_questions table
+CREATE TABLE IF NOT EXISTS favorite_questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    question_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
+    UNIQUE (user_id, question_id)
+);
+
+CREATE TABLE IF NOT EXISTS upvoted_forum_posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES forum_posts(id) ON DELETE CASCADE,
+    UNIQUE (user_id, post_id)
+);
+
+CREATE TABLE IF NOT EXISTS post_tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    english_id int,
+    FOREIGN KEY (post_id) REFERENCES forum_posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (english_id) REFERENCES english(id) ON DELETE CASCADE
+);
+
+
+
+
+-- Add indexes on the email and username columns in the users table
+CREATE INDEX idx_email ON users(email);
+CREATE INDEX idx_username ON users(username);
+
+DELIMITER //
+
+CREATE TRIGGER update_created_quizzes
+AFTER INSERT ON quizzes
+FOR EACH ROW
+BEGIN
+    UPDATE users
+    SET created_quizzes = created_quizzes + 1
+    WHERE id = NEW.user_id;
+END //
+
+CREATE TRIGGER update_created_quizzes_after_delete
+AFTER DELETE ON quizzes
+FOR EACH ROW
+BEGIN
+    UPDATE users
+    SET created_quizzes = created_quizzes - 1
+    WHERE id = OLD.user_id;
+END //
+
+DELIMITER ;
+
