f3682be1ae3be658bf1717076b42cdd3210552df
ramazanoacar <ramazan.acar@boun.edu.tr>
2024-11-25 16:09:30 +0300

    change host context

--- mobile/Quizzard/app/HostContext.tsx
+++ mobile/Quizzard/app/HostContext.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-const HostUrlContext = React.createContext("http://localhost:80"); 
-// const HostUrlContext = React.createContext("http://34.55.188.177");
+// const HostUrlContext = React.createContext("http://localhost:80");
+const HostUrlContext = React.createContext("http://34.55.188.177");
 
-// const HostContext = React.createContext("34.55.188.177"); 
-const HostContext = React.createContext("localhost"); 
-
+const HostContext = React.createContext("34.55.188.177");
+// const HostContext = React.createContext("localhost");
 
 export default HostUrlContext;
