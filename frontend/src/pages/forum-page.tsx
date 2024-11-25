9c05ee9fea8bc89130a23191a19cba41b2f6b7ce
Özgür Deniz Demir <32364679+odenizddd@users.noreply.github.com>
2024-11-25 20:41:55 +0300

    connect to forum search endpoint

--- frontend/src/pages/forum-page.tsx
+++ frontend/src/pages/forum-page.tsx
@@ -6,8 +6,9 @@ import { useState } from "react";
 export const ForumPage = () => {  
 
   const [postComponent, setPostComponent] = useState<boolean>(false);
+  const [searchInput, setSearchInput] = useState<string>("");
 
-  const { data: posts } = useFetchPosts();
+  const { data: posts } = useFetchPosts({tag:searchInput});
 
   console.log('here', posts)
 
@@ -18,7 +19,7 @@ export const ForumPage = () => {
   return (
     <div className="max-w-2xl flex flex-col gap-8 mx-auto items-center">
       <div className="w-full">
-        <input type="text" placeholder="Search posts." className="border-b-2 border-b-violet-300 focus:border-b-violet-600 w-full text-xl px-2 py-1 outline-none"/>
+        <input value={searchInput} onChange={e => {setSearchInput(e.target.value)}} type="text" placeholder="Search posts." className="border-b-2 border-b-violet-300 focus:border-b-violet-600 w-full text-xl px-2 py-1 outline-none"/>
       </div>
       <div className="flex justify-end items-end w-full">
         <button onClick={() => {setPostComponent(true)}} className="bg-violet-600 text-white p-2 px-8 rounded-lg">Create Post</button>
