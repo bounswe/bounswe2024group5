a3352f9a7bf994d2b0f82a0a0ac89f5b016a26ca
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-11-25 21:34:56 +0300

    update results page

--- frontend/src/hooks/api/attempts/update.ts
+++ frontend/src/hooks/api/attempts/update.ts
@@ -1,25 +1,27 @@
-
-
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { useContext } from "react";
 import HostContext from "../../../HostContext";
 
-type QuizAttempt = {
+export type QuizAttempt = {
 	id: number;
 	userId: number;
 	quizId: number;
 	score: number;
-	completed: boolean;
 	completedAt: string;
 	updatedAt: string;
+	completed: boolean;
+}
+
+type UpdateQuizAttemptRequest = {
+	completed: boolean;
 }
 
 export const useUpdateQuizAttempt = (id: number) => {
 	const queryClient = useQueryClient();
 	const hostUrl = useContext(HostContext);
 
-	return useMutation({
-		mutationFn: async ({ completed }: { completed: boolean }): Promise<QuizAttempt> => {
+	return useMutation<QuizAttempt, Error, UpdateQuizAttemptRequest>({
+		mutationFn: async ({ completed }): Promise<QuizAttempt> => {
 			const TOKEN = sessionStorage.getItem('token');
 			const response = await fetch(`${hostUrl}/api/quiz-attempts/${id}`, {
 				method: 'PUT',
