afb01ab874f3779ffdb07298e2895b0b936493c8
Süleyman Emir Taşan <141218519+set120120@users.noreply.github.com>
2024-11-25 11:18:14 +0300

    feat(backend): added auto-complete for Turkish words

--- backend/src/main/java/com/quizzard/quizzard/repository/TurkishRepository.java
+++ backend/src/main/java/com/quizzard/quizzard/repository/TurkishRepository.java
@@ -1,11 +1,15 @@
 package com.quizzard.quizzard.repository;
 
+import com.quizzard.quizzard.model.English;
 import com.quizzard.quizzard.model.Turkish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TurkishRepository extends JpaRepository<Turkish, Long> {
     boolean existsByWord(String word);
     Turkish findByWord(String word);
+    List<Turkish> findTop5ByWordStartingWith(String prefix);
 }
