fa7e1c729338df7982ae92ed6271d7993f1127c9
Ebrar Kızıloğlu <ebrarkiziloglu@gmail.com>
2024-12-15 01:35:52 +0300

    Add missing parameter to autocomplete api call

--- mobile/Quizzard/screens/CreateQuestionScreen.tsx
+++ mobile/Quizzard/screens/CreateQuestionScreen.tsx
@@ -140,7 +140,7 @@ const CreateQuestionScreen: React.FC<Props> = ({ navigation }) => {
         const response = await fetch(
           `${hostUrl}/api/autocomplete?prefix=${encodeURIComponent(
             currentTag
-          )}`,
+          )}&language=english`,
           {
             method: "GET",
             headers: {
