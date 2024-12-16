cb38daa178a3f45d1dd2685861b845a21e28030f
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-12-16 16:45:04 +0300

    Revert "update signs"
    This reverts commit f78e07c8d6dd1dd7f0c3a80c4d33fce129294228.

--- frontend/src/components/badges/level.tsx
+++ frontend/src/components/badges/level.tsx
@@ -8,31 +8,31 @@ import {
 } from "@tabler/icons-react";
 
 const getLevelInfo = (difficulty: number) => {
-  if (difficulty >= 0 && difficulty <= 500) {
+  if (difficulty >= 0 && difficulty < 500) {
     return {
       icon: <IconStar size={16} />,
       label: "A1",
       colors: "text-emerald-600 border-emerald-600",
     };
-  } else if (difficulty <= 1000) {
+  } else if (difficulty < 1000) {
     return {
       icon: <IconTrophy size={16} />,
       label: "A2",
       colors: "text-blue-600 border-blue-600",
     };
-  } else if (difficulty <= 1500) {
+  } else if (difficulty < 1500) {
     return {
       icon: <IconTarget size={16} />,
       label: "B1",
       colors: "text-amber-600 border-amber-600",
     };
-  } else if (difficulty <= 2000) {
+  } else if (difficulty < 2000) {
     return {
       icon: <IconFlame size={16} />,
       label: "B2",
       colors: "text-orange-600 border-orange-600",
     };
-  } else if (difficulty <= 2500) {
+  } else if (difficulty < 2500) {
     return {
       icon: <IconCrown size={16} />,
       label: "C1",
